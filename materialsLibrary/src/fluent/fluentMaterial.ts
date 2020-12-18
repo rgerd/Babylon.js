03f57022da69355ef2dabd488008e6aa0eeebadf
rogerdis <rogerdis@microsoft.com>
2020-12-18 17:32:58 -0500

    Make uniforms members

--- materialsLibrary/src/fluent/fluentMaterial.ts
+++ materialsLibrary/src/fluent/fluentMaterial.ts
@@ -13,7 +13,7 @@ import { SubMesh } from "babylonjs/Meshes/subMesh";
 import { Mesh } from "babylonjs/Meshes/mesh";
 import { Scene } from "babylonjs/scene";
 import { _TypeStore } from 'babylonjs/Misc/typeStore';
-import { Color3 } from 'babylonjs/Maths/math.color';
+import { Color3, Color4 } from 'babylonjs/Maths/math.color';
 import { BlobTextureData } from './fluentBlob';
 
 import "./fluent.fragment";
@@ -34,6 +34,71 @@ class FluentMaterialDefines extends MaterialDefines {
 }
 
 export class FluentMaterial extends PushMaterial {
+    // "Wireframe"
+    public EdgeWidth = 0.04;
+    public EdgeColor = new Color4(0.592157, 0.592157, 0.592157, 1.0);
+    public RelativeWidth = true;
+
+    // "Proximity"
+    public ProximityMaxIntensity = 0.45;
+    public ProximityFarDistance = 0.16;
+    public ProximityNearRadius = 1.5;
+    public ProximityAnisotropy = 1;
+
+    // "Selection"
+    public SelectionFuzz = 0.5;
+    public Selected = 0;
+    public SelectionFade = 0;
+    public SelectionFadeSize = 0.3;
+    public SelectedDistance = 0.08;
+    public SelectedFadeLength = 0.08;
+
+    // "Blob"
+    public BlobEnable = true;
+    public BlobPosition = new Vector3(0.5, 0.0, -0.55);
+    public BlobIntensity = 0.5;
+    public BlobNearSize = 0.025;
+    public BlobFarSize = 0.05;
+    public BlobNearDistance = 0;
+    public BlobFarDistance = 0.08;
+    public BlobFadeLength = 0.08;
+    public BlobInnerFade = 0.01;
+    public BlobPulse = 0;
+    public BlobFade = 1;
+
+    // "Blob 2"
+    public BlobEnable2 = true;
+    public BlobPosition2 = new Vector3(10, 10.1, -0.6);
+    public BlobNearSize2 = 0.025;
+    public BlobInnerFade2 = 0.1;
+    public BlobPulse2 = 0;
+    public BlobFade2 = 1;
+
+    // "Active Face"
+    public ActiveFaceDir = new Vector3(0, 0, -1);
+    public ActiveFaceUp = new Vector3(0, 1, 0);
+
+    // "Hololens Edge Fade"
+    public EnableFade = true;
+    public FadeWidth = 1.5;
+    public SmoothActiveFace = true ? 1.0 : 0.0;
+
+    // "Debug"
+    public ShowFrame = false ? 1.0 : 0.0;
+
+    // Global inputs
+    public UseGlobalLeftIndex = 1.0;
+    public UseGlobalRightIndex = 1.0;
+
+    public GlobalLeftIndexTipPosition = new Vector4(0.5, 0.0, -0.55, 1.0);
+    public GlobalRightIndexTipPosition = new Vector4(0.0, 0.0, 0.0, 1.0);
+
+    public GlobalLeftThumbTipPosition = new Vector4(0.5, 0.0, -0.55, 1.0);
+    public GlobalRightThumbTipPosition = new Vector4(0.0, 0.0, 0.0, 1.0);
+
+    public GlobalLeftIndexTipProximity = 0.0;
+    public GlobalRightIndexTipProximity = 0.0;
+
     private _blobTexture: Texture;
 
     constructor(name: string, scene: Scene) {
@@ -238,74 +303,74 @@ export class FluentMaterial extends PushMaterial {
         this._activeEffect.setMatrix("viewProjection", scene.getTransformMatrix());
         this._activeEffect.setVector3("cameraPosition", scene.activeCamera!.position);
 
-        // "Wireframe"
-        this._activeEffect.setFloat("_Edge_Width_", 0.04);
-        this._activeEffect.setColor4("_Edge_Color_", new Color3(0.592157, 0.592157, 0.592157), 1);
-        //define _Relative_Width_ true;
-
-        // "Proximity"
-        this._activeEffect.setFloat("_Proximity_Max_Intensity_", 0.45);
-        this._activeEffect.setFloat("_Proximity_Far_Distance_", 0.16);
-        this._activeEffect.setFloat("_Proximity_Near_Radius_", 1.5);
-        this._activeEffect.setFloat("_Proximity_Anisotropy_", 1);
-
-        // "Selection"
-        this._activeEffect.setFloat("_Selection_Fuzz_", 0.5);
-        this._activeEffect.setFloat("_Selected_", 0);
-        this._activeEffect.setFloat("_Selection_Fade_", 0);
-        this._activeEffect.setFloat("_Selection_Fade_Size_", 0.3);
-        this._activeEffect.setFloat("_Selected_Distance_", 0.08);
-        this._activeEffect.setFloat("_Selected_Fade_Length_", 0.08);
-
-        // "Blob"
-        this._activeEffect.setFloat("_Blob_Enable_", true ? 1.0 : 0.0);
-        this._activeEffect.setVector3("_Blob_Position_", new Vector3(0.5, 0.0, -0.55));
-        this._activeEffect.setFloat("_Blob_Intensity_", 0.5);
-        this._activeEffect.setFloat("_Blob_Near_Size_", 0.025);
-        this._activeEffect.setFloat("_Blob_Far_Size_", 0.05);
-        this._activeEffect.setFloat("_Blob_Near_Distance_", 0);
-        this._activeEffect.setFloat("_Blob_Far_Distance_", 0.08);
-        this._activeEffect.setFloat("_Blob_Fade_Length_", 0.08);
-        this._activeEffect.setFloat("_Blob_Inner_Fade_", 0.01);
-        this._activeEffect.setFloat("_Blob_Pulse_", 0);
-        this._activeEffect.setFloat("_Blob_Fade_", 1);
-
         // "Blob Texture"
         this._activeEffect.setTexture("_Blob_Texture_", this._blobTexture);
 
+        // "Wireframe"
+        this._activeEffect.setFloat("_Edge_Width_", this.EdgeWidth);
+        this._activeEffect.setColor4("_Edge_Color_", new Color3(this.EdgeColor.r, this.EdgeColor.g, this.EdgeColor.b), this.EdgeColor.a);
+        //define _Relative_Width_ true;
+
+        // "Proximity"
+        this._activeEffect.setFloat("_Proximity_Max_Intensity_", this.ProximityMaxIntensity);
+        this._activeEffect.setFloat("_Proximity_Far_Distance_", this.ProximityFarDistance);
+        this._activeEffect.setFloat("_Proximity_Near_Radius_", this.ProximityNearRadius);
+        this._activeEffect.setFloat("_Proximity_Anisotropy_", this.ProximityAnisotropy);
+
+        // "Selection"
+        this._activeEffect.setFloat("_Selection_Fuzz_", this.SelectionFuzz);
+        this._activeEffect.setFloat("_Selected_", this.Selected);
+        this._activeEffect.setFloat("_Selection_Fade_", this.SelectionFade);
+        this._activeEffect.setFloat("_Selection_Fade_Size_", this.SelectionFadeSize);
+        this._activeEffect.setFloat("_Selected_Distance_", this.SelectedDistance);
+        this._activeEffect.setFloat("_Selected_Fade_Length_", this.SelectedFadeLength);
+
+        // "Blob"
+        this._activeEffect.setFloat("_Blob_Enable_", this.BlobEnable ? 1.0 : 0.0);
+        this._activeEffect.setVector3("_Blob_Position_", this.BlobPosition);
+        this._activeEffect.setFloat("_Blob_Intensity_", this.BlobIntensity);
+        this._activeEffect.setFloat("_Blob_Near_Size_", this.BlobNearSize);
+        this._activeEffect.setFloat("_Blob_Far_Size_", this.BlobFarSize);
+        this._activeEffect.setFloat("_Blob_Near_Distance_", this.BlobNearDistance);
+        this._activeEffect.setFloat("_Blob_Far_Distance_", this.BlobFarDistance);
+        this._activeEffect.setFloat("_Blob_Fade_Length_", this.BlobFadeLength);
+        this._activeEffect.setFloat("_Blob_Inner_Fade_", this.BlobInnerFade);
+        this._activeEffect.setFloat("_Blob_Pulse_", this.BlobPulse);
+        this._activeEffect.setFloat("_Blob_Fade_", this.BlobFade);
+
         // "Blob 2"
-        this._activeEffect.setFloat("_Blob_Enable_2_", true ? 1.0 : 0.0);
-        this._activeEffect.setVector3("_Blob_Position_2_", new Vector3(10, 10.1, -0.6));
-        this._activeEffect.setFloat("_Blob_Near_Size_2_", 0.025);
-        this._activeEffect.setFloat("_Blob_Inner_Fade_2_", 0.1);
-        this._activeEffect.setFloat("_Blob_Pulse_2_", 0);
-        this._activeEffect.setFloat("_Blob_Fade_2_", 1);
+        this._activeEffect.setFloat("_Blob_Enable_2_", this.BlobEnable2 ? 1.0 : 0.0);
+        this._activeEffect.setVector3("_Blob_Position_2_", this.BlobPosition2);
+        this._activeEffect.setFloat("_Blob_Near_Size_2_", this.BlobNearSize2);
+        this._activeEffect.setFloat("_Blob_Inner_Fade_2_", this.BlobInnerFade2);
+        this._activeEffect.setFloat("_Blob_Pulse_2_", this.BlobPulse2);
+        this._activeEffect.setFloat("_Blob_Fade_2_", this.BlobFade2);
 
         // "Active Face"
-        this._activeEffect.setVector3("_Active_Face_Dir_", new Vector3(0, 0, -1));
-        this._activeEffect.setVector3("_Active_Face_Up_", new Vector3(0, 1, 0));
+        this._activeEffect.setVector3("_Active_Face_Dir_", this.ActiveFaceDir);
+        this._activeEffect.setVector3("_Active_Face_Up_", this.ActiveFaceUp);
 
         // "Hololens Edge Fade"
         //define _Enable_Fade_ true;
-        this._activeEffect.setFloat("_Fade_Width_", 1.5);
-        this._activeEffect.setFloat("_Smooth_Active_Face_", true ? 1.0 : 0.0);
+        this._activeEffect.setFloat("_Fade_Width_", this.FadeWidth);
+        this._activeEffect.setFloat("_Smooth_Active_Face_", this.SmoothActiveFace);
 
         // "Debug"
-        this._activeEffect.setFloat("_Show_Frame_", false ? 1.0 : 0.0);
+        this._activeEffect.setFloat("_Show_Frame_", this.ShowFrame);
 
 
         // Global inputs
-        this._activeEffect.setFloat("Use_Global_Left_Index", 1.0);
-        this._activeEffect.setFloat("Use_Global_Right_Index", 1.0);
+        this._activeEffect.setFloat("Use_Global_Left_Index", this.UseGlobalLeftIndex ? 1.0 : 0.0);
+        this._activeEffect.setFloat("Use_Global_Right_Index", this.UseGlobalRightIndex ? 1.0 : 0.0);
 
-        this._activeEffect.setVector4("Global_Left_Index_Tip_Position", new Vector4(0.5, 0.0, -0.55, 1.0));
-        this._activeEffect.setVector4("Global_Right_Index_Tip_Position", new Vector4(0.0, 0.0, 0.0, 1.0));
+        this._activeEffect.setVector4("Global_Left_Index_Tip_Position", this.GlobalLeftIndexTipPosition);
+        this._activeEffect.setVector4("Global_Right_Index_Tip_Position", this.GlobalRightIndexTipPosition);
 
-        this._activeEffect.setVector4("Global_Left_Thumb_Tip_Position", new Vector4(0.5, 0.0, -0.55, 1.0));
-        this._activeEffect.setVector4("Global_Right_Thumb_Tip_Position", new Vector4(0.0, 0.0, 0.0, 1.0));
+        this._activeEffect.setVector4("Global_Left_Thumb_Tip_Position", this.GlobalLeftThumbTipPosition);
+        this._activeEffect.setVector4("Global_Right_Thumb_Tip_Position", this.GlobalRightThumbTipPosition);
 
-        this._activeEffect.setFloat("Global_Left_Index_Tip_Proximity", 0.0);
-        this._activeEffect.setFloat("Global_Right_Index_Tip_Proximity", 0.0);
+        this._activeEffect.setFloat("Global_Left_Index_Tip_Proximity", this.GlobalLeftIndexTipProximity);
+        this._activeEffect.setFloat("Global_Right_Index_Tip_Proximity", this.GlobalLeftIndexTipProximity);
 
         this._afterBind(mesh, this._activeEffect);
     }
